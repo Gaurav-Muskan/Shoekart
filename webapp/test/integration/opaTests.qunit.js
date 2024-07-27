db7bddd066c944e340a0790dd7fca92e9d64a606
Gaurav Muskan <gaurav.muskan@incture.com>
2024-07-27 04:23:58 +0000

    Files are created and Login Page is made(View1)

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/projects/shoekart/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comprojects/shoekart/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
